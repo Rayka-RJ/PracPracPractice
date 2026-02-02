7bf1134249695cff2c13ab0ae8d2946520693b78
Rayka-RJ <zhangrj15@gmail.com>
2026-02-03 02:28:14 +0800

    hot 60/100; data preprocess+attention

--- Algo/LLM_foundation/my-nanogpt/data/prepare.py
+++ Algo/LLM_foundation/my-nanogpt/data/prepare.py
@@ -2,4 +2,49 @@
 
 # 下载数据的cli
 # curl -LO https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt 
+import os
+import numpy as np
 
+current_dir = os.path.dirname(__file__)
+input_file_path = os.path.join(current_dir, 'input.txt')
+
+with open(input_file_path, 'r') as f:
+    data = f.read()
+print(f'length of dataset in char: {len(data):}')
+# length of dataset in char: 1115394
+
+# vocabulary
+chars = sorted(list(set(data)))
+
+# char --> idx
+stoi = { ch:i for i,ch in enumerate(chars)}
+itos = { i:ch for i,ch in enumerate(chars)}
+
+def encode(s):
+    return [stoi[ch] for ch in s] # string --> idx
+
+def decode(l):
+    return ''.join([itos[i] for i in l]) # idx --> stirng
+
+# train/eval split
+n = len(data)
+train = data[:int(n*0.9)]
+eval = data[int(n*0.9):]
+
+train_encode = encode(train)
+eval_encode = encode(eval)
+
+# export binary file
+train_encode = np.array(train_encode, dtype=np.uint16)
+eval_encode = np.array(eval_encode, dtype=np.uint16)
+train_encode.tofile(os.path.join(current_dir,'train.bin'))
+eval_encode.tofile(os.path.join(current_dir, 'eval.bin'))
+
+# save the meta information
+import pickle
+meta = {
+    'itos':itos,
+    'stoi':stoi
+}
+with open(os.path.join(current_dir, 'meta.pkl'), 'wb') as f:
+    pickle.dump(meta, f)
--- Algo/LLM_foundation/my-nanogpt/model.py
+++ Algo/LLM_foundation/my-nanogpt/model.py
@@ -1,1 +1,110 @@
 # model
+
+import math
+from turtle import forward
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GPTConfig:    
+    def __init__(self, **kwargs) -> None:
+        self.vocal_size = 65        # vocal_size
+        self.block_size = 256       # T - sequence length
+        self.n_head = 6             # H
+        self.n_embd = 384           # C
+        self.dropout = 0.2          # dropout rate
+        self.n_layer = 6            # layer of transformer
+        self.bias = False           # layerNorm & linear
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+class LayerNorm(nn.Module):
+    def __init__(self, ndim, bias) -> None:
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+    def forward(self, input):
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, eps=1e-5)
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # q,k,v
+        self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd, bias=config.bias)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        self.n_embd = config.n_embd 
+        self.n_head = config.n_head
+        self.dropout = nn.Dropout(config.dropout)
+        # Causal Mask
+        # 把 mask 保存在模型里,但不当作可训练参数
+        self.register_buffer('bias', torch.tril(torch.zeros(config.block_size, config.block_size)).view(1,1,config.block_size, config.block_size))
+    def forward(self, x):
+        # X --> (B, T, C)
+        B, T, C = x.size() 
+        # q,k,v --> 3 * (B,T,C)
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=-1) # 按照每份XX来分，dim表示在哪个维度拆
+        
+        # multi-head (B,T,C) --> (B,T,H,E) --> (B, H, T, E)
+        q = q.view(B, T, self.n_head, self.n_embd//self.n_head).transpose(1, 2)
+        k = k.view(B, T, self.n_head, self.n_embd//self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, self.n_embd//self.n_head).transpose(1, 2)
+        
+        # attention calculation
+        attn = q @ k.transpose(-2,-1) * (1.0 / math.sqrt(k.size(-1)))
+        attn = attn.masked_fill(self.bias[:,:,:T,:T]==0, float('-inf')) # masked!
+        attn = F.softmax(attn, dim=-1)
+        attn = self.dropout(attn)
+        y = attn @ v # (B, H, T, T) @ (B, H, T, E) --> (B, H, T, E)
+
+        # # Flash attention 的版本
+        # y = F.scaled_dot_product_attention(q,k,v,attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        # (B, H, T, E) --> (B, T, C)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) 
+        y = self.dropout(y)
+        return y
+
+class MLP(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        pass
+
+class Transformer(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class GPT(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        pass
+
+'''
+1. shape和size
+# k.shape      # 属性,返回 torch.Size([B, nh, T, hs])
+# k.shape[-1]  # 64 (索引,不是方法调用)
+# k.size(-1)   # 方法,返回 int (最后一维的大小)
+
+2. transpose
+# 这两个完全一样
+k.transpose(-2, -1)  # 交换倒数第2维和倒数第1维
+k.transpose(-1, -2)  # 交换倒数第1维和倒数第2维
+
+3. causalmask
+self.bias 是固定的: (1, 1, block_size, block_size) = (1, 1, 256, 256)
+
+# 但实际输入可能只有 10 个 token
+x: (B, 10, C)
+
+# 手动实现需要切片:
+mask = self.bias[:, :, :10, :10]  # 只用前 10x10 的部分
+
+# is_causal=True 自动处理:
+# PyTorch 自动创建 10x10 的 mask,不需要我们切片
+'''
